4569052a4e91b9cd2d73615647b55a4fa83aa4a7
Łukasz Matuła <lmatula@sii.pl>
2022-03-17 19:18:45 +0100

    Create gradle project registerup

---
+++ src/main/java/pl/sii/registerup/RegisterupApplication.java
@@ -0,0 +1,13 @@
+package pl.sii.registerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterupApplication.class, args);
+	}
+
+}
