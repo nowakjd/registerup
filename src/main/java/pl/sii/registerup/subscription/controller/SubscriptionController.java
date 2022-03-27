206a17d53a9bfe6358104453bdeb91a16875ef5f
mmoleda1 <mmoleda1@sii.pl>
2022-03-27 22:22:06 +0200

    Post controller already added to code.

--- src/main/java/pl/sii/registerup/subscription/controller/SubscriptionController.java
+++ src/main/java/pl/sii/registerup/subscription/controller/SubscriptionController.java
@@ -1,4 +1,27 @@
 package pl.sii.registerup.subscription.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.sii.registerup.subscription.service.command.SubscriptionCommandService;
+import pl.sii.registerup.subscription.service.model.SubscriptionInput;
+import pl.sii.registerup.subscription.service.model.SubscriptionOutput;
+
+@RestController
+@RequestMapping
 public class SubscriptionController {
+    final SubscriptionCommandService subscriptionCommandService;
+
+
+    public SubscriptionController(SubscriptionCommandService subscriptionCommandService) {
+        this.subscriptionCommandService = subscriptionCommandService;
+    }
+
+    @PostMapping("/speakers")
+    ResponseEntity<SubscriptionOutput> createSubscription(@RequestBody SubscriptionInput subscriptionInput) {
+        return new ResponseEntity<>(subscriptionCommandService.createSubscription(subscriptionInput), HttpStatus.OK);
+    }
 }
