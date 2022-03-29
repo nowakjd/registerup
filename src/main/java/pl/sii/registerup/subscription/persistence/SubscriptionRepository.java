d208bde298969d781f8e9b6dc98c2710cb12a9e5
Tomasz Dziędziewicki <tdziedziewicki@sii.pl>
2022-03-29 15:17:29 +0200

    repository review change

--- src/main/java/pl/sii/registerup/subscription/persistence/SubscriptionRepository.java
+++ src/main/java/pl/sii/registerup/subscription/persistence/SubscriptionRepository.java
@@ -2,13 +2,9 @@ package pl.sii.registerup.subscription.persistence;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
 /**
  * This is a repository.
  */
 public interface SubscriptionRepository extends MongoRepository<SubscriptionEntity, String> {
-    SubscriptionEntity save(SubscriptionEntity subscriptionEntity);
 
-    Optional<SubscriptionEntity> findByEmail(String email);
 }
